4bbc9b31a88558869d090c500db916a827e6eb31
tcn <Convery10122@gmail.com>
2019-03-06 02:15:32 +0100

    Added file logging.

---
+++ Utilities/Logging.hpp
@@ -0,0 +1,52 @@
+/*
+    Initial author: Convery (tcn@ayria.se)
+    Started: 06-03-2018
+    License: MIT
+*/
+
+#pragma once
+#include "Variadicstring.hpp"
+#include <cstdio>
+#include <ctime>
+
+namespace Logging
+{
+    #if !defined(MODULENAME)
+        #warning No module name specified for the logging.
+        constexpr char Logfile[] = "./NoModuleName.log";
+    #else
+        constexpr char Logfile[] = "./" MODULENAME ".log";
+    #endif
+
+    // NOTE(tcn): Not threadsafe but good enough.
+    inline void toFile(const std::basic_string_view<char> Message)
+    {
+        // Open the logfile on disk and push to it.
+        if (const auto Filehandle = std::fopen(Logfile, "a"))
+        {
+            std::fwrite(Message.data(), Message.size(), 1, Filehandle);
+            std::fclose(Filehandle);
+        }
+    }
+    inline void toStream(const std::basic_string_view<char> Message)
+    {
+        std::fwrite(Message.data(), Message.size(), 1, stderr);
+        std::fflush(stderr);
+    }
+
+    // Formatted standard printing.
+    inline void Print(const char Prefix, const std::basic_string_view<char> Message)
+    {
+        const auto Now{ std::time(NULL) };
+        char Buffer[80]{};
+
+        std::strftime(Buffer, 80, "%H:%M:%S", std::localtime(&Now));
+        toFile(va("[%c][%-8s] %*s", Prefix, Buffer, Message.size(), Message.data()));
+    }
+
+    // Remove the old logfile.
+    inline void Clearlog()
+    {
+        std::remove(Logfile);
+    }
+}
