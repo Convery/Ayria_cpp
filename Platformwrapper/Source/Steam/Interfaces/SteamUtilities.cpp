6af24bfd6f5fcb83031c3fc4a9aca6edc388db84
Convery <Convery10122@gmail.com>
2019-04-10 00:46:36 +0200

    More debug information.

--- Platformwrapper/Source/Steam/Interfaces/SteamUtilities.cpp
+++ Platformwrapper/Source/Steam/Interfaces/SteamUtilities.cpp
@@ -79,6 +79,7 @@ namespace Steam
         }
         void SetOverlayNotificationPosition(uint32_t eNotificationPosition)
         {
+            Traceprint();
         }
         bool IsAPICallCompleted(uint64_t hSteamAPICall, bool *pbFailed)
         {
