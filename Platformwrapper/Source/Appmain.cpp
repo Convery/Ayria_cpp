e57dbb8dc6e77ae9d714f8810d137767e82e25b0
Convery <Convery10122@gmail.com>
2019-04-09 11:36:58 +0200

    Started adding the platformwrapper.

---
+++ Platformwrapper/Source/Appmain.cpp
@@ -0,0 +1,63 @@
+/*
+    Initial author: Convery (tcn@ayria.se)
+    Started: 09-04-2019
+    License: MIT
+*/
+
+#include "Stdinclude.hpp"
+
+// Entrypoint when loaded as a shared library.
+#if defined _WIN32
+BOOLEAN WINAPI DllMain(HINSTANCE hDllHandle, DWORD nReason, LPVOID)
+{
+    if (nReason == DLL_PROCESS_ATTACH)
+    {
+        // Ensure that Ayrias default directories exist.
+        _mkdir("./Ayria/");
+        _mkdir("./Ayria/Plugins/");
+        _mkdir("./Ayria/Assets/");
+        _mkdir("./Ayria/Local/");
+        _mkdir("./Ayria/Logs/");
+
+        // Only keep a log for this session.
+        Logging::Clearlog();
+
+        // Opt out of further notifications.
+        DisableThreadLibraryCalls(hDllHandle);
+
+        // Although useless for most users, we create a console if we get a switch.
+        #if defined(NDEBUG)
+        if (std::strstr(GetCommandLineA(), "-devcon"))
+        #endif
+        {
+            AllocConsole();
+            freopen("CONOUT$", "w", stdout);
+            freopen("CONOUT$", "w", stderr);
+            SetConsoleTitleA("Platformwrapper Console");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+        }
+
+        /* TODO(tcn): Load plugins if available. */
+    }
+
+    return TRUE;
+}
+#else
+__attribute__((constructor)) void DllMain()
+{
+    // Ensure that Ayrias default directories exist.
+    mkdir("./Ayria/", S_IRWU | S_IRWG);
+    mkdir("./Ayria/Plugins/", S_IRWXU | S_IRWXG);
+    mkdir("./Ayria/Assets/", S_IRWU | S_IRWG);
+    mkdir("./Ayria/Local/", S_IRWU | S_IRWG);
+    mkdir("./Ayria/Logs/", S_IRWU | S_IRWG);
+
+    // Only keep a log for this session.
+    Logging::Clearlog();
+}
+#endif
+
+// Entrypoint when loaded as a plugin.
+void onStartup(bool) { /* Do .text edits */ }
+void onInitialized(bool) { /* Do .data edits */ }
+bool onMessage(const void *, uint32_t) { return false; }
