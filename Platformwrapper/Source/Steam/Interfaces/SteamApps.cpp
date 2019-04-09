8a774c6ddd93321047700d3954750feec24819ec
Convery <Convery10122@gmail.com>
2019-04-09 21:48:26 +0200

    Started implementing the SteamApps interface.

---
+++ Platformwrapper/Source/Steam/Interfaces/SteamApps.cpp
@@ -0,0 +1,301 @@
+/*
+    Initial author: Convery (tcn@ayria.se)
+    Started: 09-04-2019
+    License: MIT
+*/
+
+#include "../Steam.hpp"
+
+namespace Steam
+{
+    struct SteamApps
+    {
+        int GetAppData(uint32_t nAppID, const char *pchKey, char *pchValue, int cchValueMax)
+        {
+            Traceprint();
+            return 0;
+        }
+        bool BIsSubscribed()
+        {
+            return true;
+        }
+        bool BIsLowViolence()
+        {
+            Traceprint();
+            return false;
+        }
+        bool BIsCybercafe()
+        {
+            Traceprint();
+            return false;
+        }
+        bool BIsVACBanned()
+        {
+            return false;
+        }
+        const char *GetCurrentGameLanguage()
+        {
+            Traceprint();
+            return Steam::Global.Language.c_str();
+        }
+        const char *GetAvailableGameLanguages()
+        {
+            Traceprint();
+            return Steam::Global.Language.c_str();
+        }
+        bool BIsSubscribedApp(uint32_t nAppID)
+        {
+            return true;
+        }
+        bool BIsDlcInstalled(uint32_t nAppID)
+        {
+            return true;
+        }
+        uint32_t GetEarliestPurchaseUnixTime(uint32_t nAppID)
+        {
+            Traceprint();
+            return uint32_t(Steam::Global.Startuptimestamp - 600);
+        }
+        bool BIsSubscribedFromFreeWeekend()
+        {
+            Traceprint();
+            return false;
+        }
+        int GetDLCCount()
+        {
+            Traceprint();
+            return 0;
+        }
+        bool BGetDLCDataByIndex(int iDLC, uint32_t * pAppID, bool *pbAvailable, char *pchName, int cchNameBufferSize)
+        {
+            Traceprint();
+            return false;
+        }
+        void InstallDLC(uint32_t nAppID)
+        {
+            Infoprint(va("Installing DLC %u..", nAppID));
+        }
+        void UninstallDLC(uint32_t nAppID)
+        {
+            Infoprint(va("Uninstalling DLC %u..", nAppID));
+        }
+        void RequestAppProofOfPurchaseKey(uint32_t nAppID)
+        {
+            Traceprint();
+            return;
+        }
+        bool GetCurrentBetaName(char *pchName, int cchNameBufferSize)
+        {
+            Traceprint();
+            return false;
+        }
+        bool MarkContentCorrupt(bool bMissingFilesOnly)
+        {
+            Traceprint();
+            return false;
+        }
+        uint32_t GetInstalledDepots0(uint32_t * pvecDepots, uint32_t cMaxDepots)
+        {
+            Traceprint();
+            return 0;
+        }
+        uint32_t GetAppInstallDir(uint32_t appID, char *pchFolder, uint32_t cchFolderBufferSize)
+        {
+            Traceprint();
+
+            return 0;
+        }
+        bool BIsAppInstalled(uint32_t appID)
+        {
+            return true;
+        }
+        uint32_t GetInstalledDepots1(uint32_t appID, uint32_t * pvecDepots, uint32_t cMaxDepots)
+        {
+            Traceprint();
+            return 0;
+        }
+        CSteamID GetAppOwner()
+        {
+            Traceprint();
+            return CSteamID(Steam::Global.UserID);
+        }
+        const char *GetLaunchQueryParam(const char *pchKey)
+        {
+            Traceprint();
+            return "";
+        }
+        bool GetDlcDownloadProgress(uint32_t nAppID, uint64_t * punBytesDownloaded, uint64_t * punBytesTotal)
+        {
+            Traceprint();
+            return false;
+        }
+        int GetAppBuildId()
+        {
+            Traceprint();
+            return 0;
+        }
+        uint64_t RegisterActivationCode(const char *pchActivationCode)
+        {
+            Traceprint();
+            return 0;
+        }
+    };
+
+    std::any Hackery;
+    #define Createmethod(Index, Class, Function) Hackery = &Class::Function; VTABLE[Index] = *(void **)&Hackery;
+
+    struct SteamApps001 : Interface_t
+    {
+        SteamApps001()
+        {
+            Createmethod(0, SteamApps, GetAppData);
+        }
+    };
+    struct SteamApps002 : Interface_t
+    {
+        SteamApps002()
+        {
+            Createmethod(0, SteamApps, BIsSubscribed);
+            Createmethod(1, SteamApps, BIsLowViolence);
+            Createmethod(2, SteamApps, BIsCybercafe);
+            Createmethod(3, SteamApps, BIsVACBanned);
+            Createmethod(4, SteamApps, GetCurrentGameLanguage);
+            Createmethod(5, SteamApps, GetAvailableGameLanguages);
+            Createmethod(6, SteamApps, BIsSubscribedApp);
+        };
+    };
+    struct SteamApps003 : Interface_t
+    {
+        SteamApps003()
+        {
+            Createmethod(0, SteamApps, BIsSubscribed);
+            Createmethod(1, SteamApps, BIsLowViolence);
+            Createmethod(2, SteamApps, BIsCybercafe);
+            Createmethod(3, SteamApps, BIsVACBanned);
+            Createmethod(4, SteamApps, GetCurrentGameLanguage);
+            Createmethod(5, SteamApps, GetAvailableGameLanguages);
+            Createmethod(6, SteamApps, BIsSubscribedApp);
+            Createmethod(7, SteamApps, BIsDlcInstalled);
+        };
+    };
+    struct SteamApps004 : Interface_t
+    {
+        SteamApps004()
+        {
+            Createmethod(0, SteamApps, BIsSubscribed);
+            Createmethod(1, SteamApps, BIsLowViolence);
+            Createmethod(2, SteamApps, BIsCybercafe);
+            Createmethod(3, SteamApps, BIsVACBanned);
+            Createmethod(4, SteamApps, GetCurrentGameLanguage);
+            Createmethod(5, SteamApps, GetAvailableGameLanguages);
+            Createmethod(6, SteamApps, BIsSubscribedApp);
+            Createmethod(7, SteamApps, BIsDlcInstalled);
+            Createmethod(8, SteamApps, GetEarliestPurchaseUnixTime);
+            Createmethod(9, SteamApps, BIsSubscribedFromFreeWeekend);
+            Createmethod(10, SteamApps, GetDLCCount);
+            Createmethod(11, SteamApps, BGetDLCDataByIndex);
+            Createmethod(12, SteamApps, InstallDLC);
+            Createmethod(13, SteamApps, UninstallDLC);
+        };
+    };
+    struct SteamApps005 : Interface_t
+    {
+        SteamApps005()
+        {
+            Createmethod(0, SteamApps, BIsSubscribed);
+            Createmethod(1, SteamApps, BIsLowViolence);
+            Createmethod(2, SteamApps, BIsCybercafe);
+            Createmethod(3, SteamApps, BIsVACBanned);
+            Createmethod(4, SteamApps, GetCurrentGameLanguage);
+            Createmethod(5, SteamApps, GetAvailableGameLanguages);
+            Createmethod(6, SteamApps, BIsSubscribedApp);
+            Createmethod(7, SteamApps, BIsDlcInstalled);
+            Createmethod(8, SteamApps, GetEarliestPurchaseUnixTime);
+            Createmethod(9, SteamApps, BIsSubscribedFromFreeWeekend);
+            Createmethod(10, SteamApps, GetDLCCount);
+            Createmethod(11, SteamApps, BGetDLCDataByIndex);
+            Createmethod(12, SteamApps, InstallDLC);
+            Createmethod(13, SteamApps, UninstallDLC);
+            Createmethod(14, SteamApps, RequestAppProofOfPurchaseKey);
+            Createmethod(15, SteamApps, GetCurrentBetaName);
+            Createmethod(16, SteamApps, MarkContentCorrupt);
+            Createmethod(17, SteamApps, GetInstalledDepots0);
+            Createmethod(18, SteamApps, GetAppInstallDir);
+            Createmethod(19, SteamApps, BIsAppInstalled);
+        };
+    };
+    struct SteamApps006 : Interface_t
+    {
+        SteamApps006()
+        {
+            Createmethod(0, SteamApps, BIsSubscribed);
+            Createmethod(1, SteamApps, BIsLowViolence);
+            Createmethod(2, SteamApps, BIsCybercafe);
+            Createmethod(3, SteamApps, BIsVACBanned);
+            Createmethod(4, SteamApps, GetCurrentGameLanguage);
+            Createmethod(5, SteamApps, GetAvailableGameLanguages);
+            Createmethod(6, SteamApps, BIsSubscribedApp);
+            Createmethod(7, SteamApps, BIsDlcInstalled);
+            Createmethod(8, SteamApps, GetEarliestPurchaseUnixTime);
+            Createmethod(9, SteamApps, BIsSubscribedFromFreeWeekend);
+            Createmethod(10, SteamApps, GetDLCCount);
+            Createmethod(11, SteamApps, BGetDLCDataByIndex);
+            Createmethod(12, SteamApps, InstallDLC);
+            Createmethod(13, SteamApps, UninstallDLC);
+            Createmethod(14, SteamApps, RequestAppProofOfPurchaseKey);
+            Createmethod(15, SteamApps, GetCurrentBetaName);
+            Createmethod(16, SteamApps, MarkContentCorrupt);
+            Createmethod(17, SteamApps, GetInstalledDepots1);
+            Createmethod(18, SteamApps, GetAppInstallDir);
+            Createmethod(19, SteamApps, BIsAppInstalled);
+            Createmethod(20, SteamApps, GetAppOwner);
+            Createmethod(21, SteamApps, GetLaunchQueryParam);
+        };
+    };
+    struct SteamApps007 : Interface_t
+    {
+        SteamApps007()
+        {
+            Createmethod(0, SteamApps, BIsSubscribed);
+            Createmethod(1, SteamApps, BIsLowViolence);
+            Createmethod(2, SteamApps, BIsCybercafe);
+            Createmethod(3, SteamApps, BIsVACBanned);
+            Createmethod(4, SteamApps, GetCurrentGameLanguage);
+            Createmethod(5, SteamApps, GetAvailableGameLanguages);
+            Createmethod(6, SteamApps, BIsSubscribedApp);
+            Createmethod(7, SteamApps, BIsDlcInstalled);
+            Createmethod(8, SteamApps, GetEarliestPurchaseUnixTime);
+            Createmethod(9, SteamApps, BIsSubscribedFromFreeWeekend);
+            Createmethod(10, SteamApps, GetDLCCount);
+            Createmethod(11, SteamApps, BGetDLCDataByIndex);
+            Createmethod(12, SteamApps, InstallDLC);
+            Createmethod(13, SteamApps, UninstallDLC);
+            Createmethod(14, SteamApps, RequestAppProofOfPurchaseKey);
+            Createmethod(15, SteamApps, GetCurrentBetaName);
+            Createmethod(16, SteamApps, MarkContentCorrupt);
+            Createmethod(17, SteamApps, GetInstalledDepots1);
+            Createmethod(18, SteamApps, GetAppInstallDir);
+            Createmethod(19, SteamApps, BIsAppInstalled);
+            Createmethod(20, SteamApps, GetAppOwner);
+            Createmethod(21, SteamApps, GetLaunchQueryParam);
+            Createmethod(22, SteamApps, GetDlcDownloadProgress);
+            Createmethod(23, SteamApps, GetAppBuildId);
+            Createmethod(24, SteamApps, RegisterActivationCode);
+        };
+    };
+
+    struct Steamappsloader
+    {
+        Steamappsloader()
+        {
+            Registerinterface(Interfacetype_t::APPS, "SteamApps001", new SteamApps001());
+            Registerinterface(Interfacetype_t::APPS, "SteamApps002", new SteamApps002());
+            Registerinterface(Interfacetype_t::APPS, "SteamApps003", new SteamApps003());
+            Registerinterface(Interfacetype_t::APPS, "SteamApps004", new SteamApps004());
+            Registerinterface(Interfacetype_t::APPS, "SteamApps005", new SteamApps005());
+            Registerinterface(Interfacetype_t::APPS, "SteamApps006", new SteamApps006());
+            Registerinterface(Interfacetype_t::APPS, "SteamApps007", new SteamApps007());
+        }
+    };
+    static Steamappsloader Interfaceloader{};
+}
