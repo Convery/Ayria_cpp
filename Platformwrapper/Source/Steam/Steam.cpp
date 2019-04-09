40a4680fbeaa17bbc0da9fbeeff70f72fbac52ba
Convery <Convery10122@gmail.com>
2019-04-09 21:27:33 +0200

    Largely implemented Steam_Init

--- Platformwrapper/Source/Steam/Steam.cpp
+++ Platformwrapper/Source/Steam/Steam.cpp
@@ -7,6 +7,9 @@
 #include "../Stdinclude.hpp"
 #include "Steam.hpp"
 
+// Keep the global state together.
+namespace Steam { Globalstate_t Global{}; }
+
 // Exported Steam interface.
 extern "C"
 {
@@ -15,13 +18,121 @@ extern "C"
     {
         Traceprint();
 
+        // Start tracking availability.
+        Steam::Global.Startuptimestamp = time(NULL);
+
         // Start processing the IPC separately.
         std::thread(Steam::InitializeIPC).detach();
 
+        // Modern games provide the ApplicationID via SteamAPI_RestartAppIfNecessary.
+        // While legacy/dedis have hardcoded steam_apis. Thus we need a configuration file.
+        if (Steam::Global.ApplicationID == 0)
+        {
+            // Check for configuration files.
+            if (auto Filehandle = std::fopen("steam_appid.txt", "r"))
+            {
+                std::fscanf(Filehandle, "%u", &Steam::Global.ApplicationID);
+                std::fclose(Filehandle);
+            }
+            if (auto Filehandle = std::fopen("ayria_appid.txt", "r"))
+            {
+                std::fscanf(Filehandle, "%u", &Steam::Global.ApplicationID);
+                std::fclose(Filehandle);
+            }
 
+            // Else we need to at least warn the user, although it should be an error.
+            if (Steam::Global.ApplicationID == 0)
+            {
+                Errorprint("Platformwrapper could not find the games Application ID.");
+                Errorprint("This can cause a lot of errors, contact the developer.");
+                Errorprint("Alternatively provide a \"steam_appid.txt\" or \"ayria_appid.txt\"");
 
+                Steam::Global.ApplicationID = Hash::FNV1a_32("Ayria");
+            }
+        }
 
+        // Query the Ayria platform for the username and localization.
+        if (true /* TODO(tcn): Update when Ayria is done. */)
+        {
+            Steam::Global.Username = "Ayria";
+            Steam::Global.Language = "english";
+        }
 
+        // Query the Steam platform for installation-location.
+        #if defined(_WIN32)
+        {
+            HKEY Registrykey;
+            constexpr auto Steamregistry = Build::is64bit ? "Software\\Wow6432Node\\Valve\\Steam" : "Software\\Valve\\Steam";
+
+            if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, Steamregistry, 0, KEY_QUERY_VALUE, &Registrykey) == ERROR_SUCCESS)
+            {
+                unsigned long Size = 260; unsigned char Buffer[260]{};
+                RegQueryValueExA(Registrykey, "InstallPath", NULL, NULL, Buffer, &Size);
+                Steam::Global.Path = (char *)Buffer;
+                RegCloseKey(Registrykey);
+            }
+        }
+        #endif
+
+        // Some legacy applications query the application info from the environment.
+        SetEnvironmentVariableA("SteamAppId", va("%lu", Steam::Global.ApplicationID).c_str());
+        SetEnvironmentVariableA("SteamGameId", va("%llu", Steam::Global.ApplicationID & 0xFFFFFF).c_str());
+        #if defined(_WIN32)
+        {
+            HKEY Registrykey;
+            if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Valve\\Steam\\ActiveProcess", 0, KEY_QUERY_VALUE, &Registrykey) == ERROR_SUCCESS)
+            {
+                DWORD ProcessID = GetCurrentProcessId();
+                DWORD UserID = Steam::Global.UserID & 0xFFFFFFFF;
+
+                std::string Clientpath32 = va("%s\\steamclient.dll", Steam::Global.Path.c_str());
+                std::string Clientpath64 = va("%s\\steamclient64.dll", Steam::Global.Path.c_str());
+
+                RegSetValueExA(Registrykey, "pid", NULL, REG_DWORD, (LPBYTE)&ProcessID, sizeof(DWORD));
+                RegSetValueExA(Registrykey, "ActiveUser", NULL, REG_DWORD, (LPBYTE)&UserID, sizeof(DWORD));
+                RegSetValueExA(Registrykey, "Universe", NULL, REG_SZ, (LPBYTE)"Public", (DWORD)std::strlen("Public") + 1);
+                RegSetValueExA(Registrykey, "SteamClientDll", NULL, REG_SZ, (LPBYTE)Clientpath32.c_str(), (DWORD)Clientpath32.length() + 1);
+                RegSetValueExA(Registrykey, "SteamClientDll64", NULL, REG_SZ, (LPBYTE)Clientpath64.c_str(), (DWORD)Clientpath64.length() + 1);
+
+                RegCloseKey(Registrykey);
+            }
+
+            DWORD Disposition;
+            if (RegCreateKeyExA(HKEY_CURRENT_USER, va("Software\\Valve\\Steam\\Apps\\%i", Steam::Global.ApplicationID).c_str(), 0, NULL, 0, KEY_WRITE, NULL, &Registrykey, &Disposition) == ERROR_SUCCESS)
+            {
+                DWORD Running = TRUE;
+
+                RegSetValueExA(Registrykey, "Installed", NULL, REG_DWORD, (LPBYTE)&Running, sizeof(DWORD));
+                RegSetValueExA(Registrykey, "Running", NULL, REG_DWORD, (LPBYTE)&Running, sizeof(DWORD));
+
+                RegCloseKey(Registrykey);
+            }
+        }
+        #endif
+
+        // We don't emulate Steams overlay, but users will have to request it.
+        #if defined(_WIN32)
+        if (std::strstr(GetCommandLineA(), "-overlay"))
+        {
+            constexpr auto *Gameoverlay = Build::is64bit ? "gameoverlayrenderer64.dll" : "gameoverlayrenderer.dll";
+            constexpr auto *Clientlibrary = Build::is64bit ? "steamclient64.dll" : "steamclient.dll";
+
+            SetDllDirectoryA(Steam::Global.Path.c_str());
+            LoadLibraryA(va("%s\\%s", Steam::Global.Path.c_str(), Gameoverlay).c_str());
+            LoadLibraryA(va("%s\\%s", Steam::Global.Path.c_str(), Clientlibrary).c_str());
+        }
+        #endif
+
+        // Finally initialize the interfaces by module.
+        if (!Steam::Scanforinterfaces("interfaces.txt") && /* TODO(tcn): Parse interfaces from cahce. */
+            !Steam::Scanforinterfaces("steam_api.bak")  && !Steam::Scanforinterfaces("steam_api64.bak") &&
+            !Steam::Scanforinterfaces("steam_api.dll")  && !Steam::Scanforinterfaces("steam_api64.dll") &&
+            !Steam::Scanforinterfaces("steam_api.so")   && !Steam::Scanforinterfaces("steam_api64.so"))
+        {
+            Errorprint("Platformwrapper could not find the games interface version.");
+            Errorprint("This can cause a lot of errors, contact the developer.");
+            Errorprint("Alternatively provide a \"interfaces.txt\"");
+        }
 
         return true;
     }
@@ -29,7 +140,7 @@ extern "C"
     EXPORT_ATTR void SteamAPI_Shutdown() { Traceprint(); }
     EXPORT_ATTR bool SteamAPI_IsSteamRunning() { Traceprint(); return true; }
     EXPORT_ATTR const char *SteamAPI_GetSteamInstallPath() { Traceprint(); return nullptr; }
-    EXPORT_ATTR bool SteamAPI_RestartAppIfNecessary(uint32_t unOwnAppID) { Traceprint(); return false; }
+    EXPORT_ATTR bool SteamAPI_RestartAppIfNecessary(uint32_t unOwnAppID) { Steam::Global.ApplicationID = unOwnAppID; return false; }
 
     // Callback management.
     EXPORT_ATTR void SteamAPI_RunCallbacks() { Traceprint(); }
