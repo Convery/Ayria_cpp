82cea83d1f8a5b90b298dccec5c32bb9b614cf6c
Convery <Convery10122@gmail.com>
2019-05-09 22:42:55 +0200

    Hooking improvements.

--- Utilities/Simplehook.hpp
+++ Utilities/Simplehook.hpp
@@ -32,24 +32,33 @@ namespace Simplehook
                 // Simple stomp-hooking.
                 if constexpr (sizeof(void *) == sizeof(uint64_t))
                 {
-                    // JMP [RIP + 0]
-                    std::memcpy((uint8_t *)Location, "\xFF\x25", 2);
-                    std::memcpy((uint8_t *)Location + 2, "\x00\x00\x00\x00", 4);
-                    std::memcpy((uint8_t *)Location + 6, &Target, sizeof(size_t));
+                    // JMP [RIP + 0], Target
+                    *(uint16_t *)((uint8_t *)Location + 0) = 0x25FF;
+                    *(uint32_t *)((uint8_t *)Location + 2) = 0x000000000;
+                    *(uint64_t *)((uint8_t *)Location + 6) = (size_t)&Target;
                 }
                 else
                 {
-                    // JMP short
+                    // JMP short Target
                     *(uint8_t *)((uint8_t *)Location + 0) = 0xE9;
                     *(size_t  *)((uint8_t *)Location + 1) = ((size_t)Target - ((size_t)Location + 5));
                 }
             }
             Memprotect::Protectrange(Location, 14, Protection);
         }
-        void Installhook(std::uintptr_t Target, std::uintptr_t Location = 0)
+        void Installhook(std::uintptr_t Target, std::uintptr_t Location)
         {
             return Installhook(reinterpret_cast<void *>(Target), reinterpret_cast<void *>(Location));
         }
+        void Installhook(void *Target, std::uintptr_t Location)
+        {
+            return Installhook(reinterpret_cast<void *>(Target), reinterpret_cast<void *>(Location));
+        }
+        void Installhook(std::uintptr_t Target, void *Location = nullptr)
+        {
+            return Installhook(reinterpret_cast<void *>(Target), reinterpret_cast<void *>(Location));
+        }
+
         void Removehook()
         {
             if (Savedlocation)
