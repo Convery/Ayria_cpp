1fd6103b26d317eedf36c6845fe04d036e5231b1
Convery <Convery10122@gmail.com>
2019-09-28 12:35:57 +0200

    Clearer Tencent implementation.

--- Platformwrapper/Source/Tencent/Tencent.cpp
+++ Platformwrapper/Source/Tencent/Tencent.cpp
@@ -7,196 +7,262 @@
 #include <Stdinclude.hpp>
 #include "../Common/Common.hpp"
 
-// Tencent variables, should be fetched in the background.
-auto Integerstore = []() { std::array<uint32_t, 64> ${}; $[1] = 42; $[18] = 1004; return $; }();
-auto Stringstore = []()
+namespace Tencent
 {
-    std::array<std::string, 64> ${};
-    $[7] = "Appticket";
-    $[22] = "PlatformID";
-    $[24] = "Tencentticket";
-    $[25] = "OpenIDLoginKey";
-    $[27] = "Unknown";
-    return $;
-}();
-auto Arraystore = []()
-{
-    std::array<std::vector<std::string>, 64> ${};
-    $[8] = { "auth3.qq.com:3074", "lsg.qq.com:3074", "1004" };
-    return $;
-}();
-struct Tencentcore
-{
-    virtual void Ctor() { Traceprint(); }
-    virtual void Initialize() { Traceprint(); }
-    virtual void Unknown0() { Traceprint(); }
-    virtual void Unknown1() { Traceprint(); }
-    virtual void Unknown2() { Traceprint(); }
-    virtual int Shutdown_notify0() { Traceprint(); return 1; }
-    virtual int Shutdown_notify1() { Traceprint(); return 1; }
-    virtual void Shutdown_notify2() { Traceprint(); }
-    virtual void Unknown3() { Traceprint(); }
-    virtual bool getInteger(uint32_t Index, uint32_t *Buffer)
+    // These should probably be fetched in the background.
+    std::unordered_map<uint32_t, std::wstring> Widestrings
     {
-        *Buffer = Integerstore[Index];
+        { 14, L"Appname" },     // Probably the main process.
+        { 15, L"Windowname" },  // Used for Sendmessage as well.
+        { 16, L"PASS:-q" },     // Arguments passed to command-line, "-q %u -k %s -s %s -j %s -t %d"
+    };
+    std::unordered_map<uint32_t, std::string> Bytestrings
+    {
+        { 3, "7" },             // Game-signature length.
+        { 4, "Gamesig" },       // Also used for QQ stuff.
+        { 5, "Loginkey" },      // Seems to be more of a SessionID.
+        { 7, "Appticket" },     // For authentication.
+        { 8, "auth3.qq.com:3074;lsg.qq.com:3074;1004" },   // Authentication-info, last ID being appID.
+        { 22, "PlatformID" },   //
+        { 24, "Tencentticket" },//
+        { 25, "OpenIDKey" },    // OpenID's standardized implementation.
+        { 26, "7" },            // Jump-signature length.
+        { 27, "Jumpsig" },      // Seems to be anti-cheat related.
+    };
+    std::unordered_map<uint32_t, int32_t> Integers{};
+    std::unordered_map<uint32_t, uint32_t> DWORDs
+    {
+        { 1, 42 },          // Unknown, seems unused.
+        { 5, 0x7F000001 },  // IP-address.
+        { 7, 38 },          // Authentication-info size.
+        { 18, 1337 },       // UserID.
+    };
 
-        Debugprint(va("Tencent_INT[%u] => %u", Index, *Buffer));
-        return true;
-    }
-    virtual bool getString(uint32_t Index, char *Buffer, uint32_t *Size)
+    void Sendmessage(WPARAM wParam, LPARAM lParam)
     {
-        if(Buffer) // If a buffer is provided, fill it.
+        auto Result = FindWindowW(L"static", NULL);
+        while(Result)
         {
-            std::memcpy(Buffer, Stringstore[Index].c_str(), std::min(*Size, (uint32_t)Stringstore[Index].size()));
-            Debugprint(va("Tencent_STRING[%u] => %s", Index, Buffer));
+            PostMessageW(Result, 0xBD2u, wParam, lParam);
+            Result = FindWindowExW(NULL, Result, L"static", NULL);
+        }
+    }
+    struct Interface
+    {
+        // TODO(tcn): Implement these when needed.
+        virtual BOOL getLogininfo(void *Loginstruct)
+        {
+            Traceprint();
+
+            // Notify listeners.
+            Sendmessage(1 | 0x650000, 0);
+
+            return true;
+        }
+        virtual BOOL Initialize()
+        {
+            Traceprint();
+
+            // Notify listeners.
+            Sendmessage(1 | 0x640000, 0);
+
+            return true;
+        }
+        virtual BOOL Restart() { Traceprint(); return true; }
+        virtual BOOL Uninitialize() { Traceprint(); return true; }
+        virtual BOOL setEventhandler(void *Eventhandler) { Traceprint(); return true; }
+        virtual void ReleaseTENIO() { Traceprint(); }
+        virtual BOOL Notifyshutdown() { Traceprint(); return true; }
+        virtual void *Closehandle() { Traceprint(); return this; }
+
+        // Global info.
+        virtual BOOL getInteger(uint32_t Tag, int32_t *Buffer)
+        {
+            Debugprint(va("%s(%u)", __FUNCTION__, Tag));
+
+            auto Result = Integers.find(Tag);
+            if(Buffer && Result != Integers.end())
+                *Buffer = Result->second;
+
+            // Notify listeners.
+            Sendmessage(1 | (Tag << 0x10), 0);
+
+            return Result != Integers.end();
+        }
+        virtual BOOL getDWORD(uint32_t Tag, uint32_t *Buffer)
+        {
+            return getDWORD_(Tag, Buffer, 0);
+        }
+        virtual BOOL getBytestring(uint32_t Tag, uint8_t *Buffer, uint32_t *Size)
+        {
+            return getBytestring_(Tag, Buffer, Size, 0);
+        }
+        virtual BOOL getWidestring(uint32_t Tag, wchar_t *Buffer, uint32_t *Size)
+        {
+            return getWidestring_(Tag, Buffer, Size, 0);
         }
 
-        // Always set the total size.
-        *Size = Stringstore[Index].size();
-
-        return true;
-    }
-    virtual void Unknown4() { Traceprint(); }
-    virtual bool getArray(uint32_t Index, char *Buffer, uint32_t *Size, uint32_t UserID)
-    {
-        std::string Readable{};
-        for(const auto &Item : Arraystore[Index])
+        // User info. NOTE(tcn): '_' because it needs to have a different name or CL changes the layout.
+        virtual BOOL getBytestring_(uint32_t Tag, uint8_t *Buffer, uint32_t *Size, uint32_t UserID)
         {
-            Readable += Item;
-            Readable += ";";
-        }
-        // Remove the last ';'
-        if(!Readable.empty()) Readable.pop_back();
+            auto Result = Bytestrings.find(Tag);
+            Debugprint(va("%s(%u)", __FUNCTION__, Tag));
+            if(Result == Bytestrings.end()) return FALSE;
 
-        if(Buffer) // If a buffer is provided, fill it.
+            // If a buffer is provided, fill it.
+            if(Buffer) std::memcpy(Buffer, Result->second.c_str(), std::min(size_t(*Size), Result->second.size()));
+
+            // Always set the total size.
+            *Size = Result->second.size();
+
+            // Notify listeners.
+            Sendmessage(1 | (Tag << 0x10), UserID != 0);
+
+            return TRUE;
+        }
+        virtual BOOL getWidestring_(uint32_t Tag, wchar_t *Buffer, uint32_t *Size, uint32_t UserID)
         {
-            std::memcpy(Buffer, Readable.c_str(), std::min(*Size, (uint32_t)Readable.size()));
-            Debugprint(va("Tencent_ARRAY[%u] => %s", Index, Buffer));
+            auto Result = Widestrings.find(Tag);
+            Debugprint(va("%s(%u)", __FUNCTION__, Tag));
+            if(Result == Widestrings.end()) return FALSE;
+
+            // If a buffer is provided, fill it.
+            if(Buffer) std::wmemcpy(Buffer, Result->second.c_str(), std::min(size_t(*Size >> 1), Result->second.size()));
+
+            // Always set the total size.
+            *Size = Result->second.size() * sizeof(wchar_t);
+
+            // Notify listeners.
+            Sendmessage(1 | (Tag << 0x10), UserID != 0);
+
+            return TRUE;
         }
+        virtual BOOL getDWORD_(uint32_t Tag, uint32_t *Buffer, uint32_t UserID)
+        {
+            Debugprint(va("%s(%u)", __FUNCTION__, Tag));
 
-        // Always set the total size.
-        *Size = Readable.size();
+            auto Result = DWORDs.find(Tag);
+            if(Buffer && Result != DWORDs.end())
+                *Buffer = Result->second;
 
-        return true;
-    }
-    virtual void Unknown5() { Traceprint(); }
-    virtual bool getArraysize(uint32_t Index, uint32_t *Buffer, uint32_t UserID)
+            // Notify listeners.
+            Sendmessage(1 | (Tag << 0x10), UserID != 0);
+
+            return Result != DWORDs.end();
+        }
+    };
+
+    // Temporary implementation of the Tensafe interface, TODO(tcn): Give it; it's own module?
+    struct Cryptoblock
     {
-        // Off by one in the SDK?
-        return getArray(Index + 1, nullptr, Buffer, UserID);
-    }
-};
-
-// Temporary implementation of the Tensafe interface, TODO(tcn): Give it; it's own module?
-struct TencentCryptoblock
-{
-    uint32_t Command;
-    uint8_t *Inputbuffer;
-    uint32_t Inputlength;
-    uint8_t *Outputbuffer;
-    uint32_t Outputlength;
-};
-struct TencentPacketheader
-{
-    uint32_t Unknown;
-    uint32_t SequenceID;
-    uint32_t CRC32Hash;
-    uint32_t Payloadlength;
-    uint32_t Packettype;
-};
-struct TencentAnticheat
-{
-    uint32_t UserID;
-    uint32_t Uin;
-    uint32_t Gameversion;
-    uint64_t pSink;
-    time_t Startuptime;
-    uint32_t Configneedsreload;
-    char Outgoingpacket[512];
-    char Incomingpacket[512];
-    uint32_t Packetssent;
-    uint32_t Unk5;
-    uint32_t Unk6;
-    uint32_t Unk7;
-    uint32_t SequenceID;
-    uint32_t Unk8;
-    char Config[1030];
-
-    virtual size_t setInit(void *Info)
+        uint32_t Command;
+        uint8_t *Inputbuffer;
+        uint32_t Inputlength;
+        uint8_t *Outputbuffer;
+        uint32_t Outputlength;
+    };
+    struct Packetheader
     {
-        Traceprint();
-        Uin = ((uint32_t *)Info)[0];
-        Gameversion = ((uint32_t *)Info)[1];
-        pSink = *(size_t *)Info + sizeof(uint64_t);
-
-        return 1;
-    }
-    virtual size_t onLogin()
+        uint32_t Unknown;
+        uint32_t SequenceID;
+        uint32_t CRC32Hash;
+        uint32_t Payloadlength;
+        uint32_t Packettype;
+    };
+    struct Anticheat
     {
-        time(&Startuptime);
-        srand(Startuptime & 0xFFFFFFFF);
-        UserID = rand();
+        uint32_t UserID;
+        uint32_t Uin;
+        uint32_t Gameversion;
+        uint64_t pSink;
+        time_t Startuptime;
+        uint32_t Configneedsreload;
+        char Outgoingpacket[512];
+        char Incomingpacket[512];
+        uint32_t Packetssent;
+        uint32_t Unk5;
+        uint32_t Unk6;
+        uint32_t Unk7;
+        uint32_t SequenceID;
+        uint32_t Unk8;
+        char Config[1030];
 
-        // TODO: Send to server.
+        virtual size_t setInit(void *Info)
+        {
+            Traceprint();
+            Uin = ((uint32_t *)Info)[0];
+            Gameversion = ((uint32_t *)Info)[1];
+            pSink = *(size_t *)Info + sizeof(uint64_t);
 
-        return 1;
-    }
-    virtual size_t Encrypt(TencentCryptoblock *Block)
-    {
-        Traceprint();
+            return 1;
+        }
+        virtual size_t onLogin()
+        {
+            time(&Startuptime);
+            srand(Startuptime & 0xFFFFFFFF);
+            UserID = rand();
 
-        /*
-            Too tired to implement.
-        */
+            // TODO: Send to server.
 
-        return 1;
-    }
-    virtual size_t isCheatpacket(uint32_t Command)
-    {
-        Traceprint();
+            return 1;
+        }
+        virtual size_t Encrypt(Cryptoblock *Block)
+        {
+            Traceprint();
 
-        /*
-            Incomingpacket =
-            {
-                uint16_t Header;
-                uint16_t Commandcount;
-                uint32_t Commands[];
-                ...
-            };
+            /*
+                Too tired to implement.
+            */
 
-            for c in Commands => return c == Command;
-        */
+            return 1;
+        }
+        virtual size_t isCheatpacket(uint32_t Command)
+        {
+            Traceprint();
 
-        return 1;
-    }
-    virtual size_t Decrypt(TencentCryptoblock *Block)
-    {
-        Traceprint();
+            /*
+                Incomingpacket =
+                {
+                    uint16_t Header;
+                    uint16_t Commandcount;
+                    uint32_t Commands[];
+                    ...
+                };
 
-        /*
-            Too tired to implement.
-        */
+                for c in Commands => return c == Command;
+            */
 
-        return 1;
-    }
-    virtual size_t Release()
-    {
-        Traceprint();
+            return 1;
+        }
+        virtual size_t Decrypt(Cryptoblock *Block)
+        {
+            Traceprint();
 
-        /*
-            Call Dtor internally.
-        */
+            /*
+                Too tired to implement.
+            */
 
-        return 1;
-    }
-};
+            return 1;
+        }
+        virtual size_t Release()
+        {
+            Traceprint();
+
+            /*
+                Call Dtor internally.
+            */
+
+            return 1;
+        }
+    };
+}
 
 extern "C"
 {
     // Tencent anti-cheat initialization override.
     EXPORT_ATTR void *CreateObj(int Type)
     {
-        return new TencentAnticheat();
+        return new Tencent::Anticheat();
     }
 
     // Tencent OpenID resolving override.
@@ -210,11 +276,7 @@ extern "C"
     EXPORT_ATTR void Invoke(uint32_t GameID, void **Interface)
     {
         Debugprint(va("Initializing Tencent for game %u", GameID));
-        *Interface = new Tencentcore();
-
-        // TODO(tcn): Name the indexes.
-        Arraystore[8][2] = va("%u", GameID);
-        Integerstore[18] = GameID;
+        *Interface = new Tencent::Interface();
     }
 }
 
