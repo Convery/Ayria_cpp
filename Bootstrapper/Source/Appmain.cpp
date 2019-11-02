8763e0a72f6f1e4081ef6b9f1fc2da0f570126f4
Convery <Convery10122@gmail.com>
2019-11-02 12:52:49 +0100

    Reworked the TLS bootstrapping system.

--- Bootstrapper/Source/Appmain.cpp
+++ Bootstrapper/Source/Appmain.cpp
@@ -6,71 +6,63 @@
 
 #include "Stdinclude.hpp"
 
-size_t OriginalTLS{};
 void Loadallplugins();
+#define Writeptr(where, what) { \
+auto Protection = Memprotect::Unprotectrange(where, sizeof(size_t)); \
+*(size_t *)where = (size_t)what; Memprotect::Protectrange(where, sizeof(size_t), Protection); }
 
-// Utility functions.
-size_t AddressofTLS()
+// Saved directory.
+std::vector<size_t> OriginalTLS{};
+
+// Parse the header to get the directory.
+PIMAGE_TLS_DIRECTORY getTLSDirectory()
 {
     // Module(NULL) gets the host application.
-    HMODULE Modulehandle = GetModuleHandleA(NULL);
-    if(!Modulehandle) return 0;
+    const HMODULE Modulehandle = GetModuleHandleA(NULL);
+    if (!Modulehandle) std::abort();
 
     // Traverse the PE header.
-    PIMAGE_DOS_HEADER DOSHeader = (PIMAGE_DOS_HEADER)Modulehandle;
-    PIMAGE_NT_HEADERS NTHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)Modulehandle + DOSHeader->e_lfanew);
-    IMAGE_DATA_DIRECTORY TLSDirectory = NTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS];
+    const PIMAGE_DOS_HEADER DOSHeader = (PIMAGE_DOS_HEADER)Modulehandle;
+    const PIMAGE_NT_HEADERS NTHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)Modulehandle + DOSHeader->e_lfanew);
+    const IMAGE_DATA_DIRECTORY Directory = NTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS];
 
-    // Get the callback address.
-    if(!TLSDirectory.VirtualAddress) return 0;
-    return size_t(((size_t *)((DWORD_PTR)Modulehandle + TLSDirectory.VirtualAddress))[3]);
+    return (PIMAGE_TLS_DIRECTORY)((DWORD_PTR)Modulehandle + Directory.VirtualAddress);
+}
+
+// Callback from Ntdll::CallTlsInitializers
+void WINAPI Dummycallback(PVOID, DWORD, PVOID) {}
+void WINAPI TLSCallback(PVOID a, DWORD b, PVOID c)
+{
+    const auto Directory = getTLSDirectory();
+
+    // Disable callbacks while loading plugins.
+    Writeptr(Directory->AddressOfCallBacks, 0);
+
+    // Load any plugins found in ./Ayria/
+    Loadallplugins();
+
+    // Restore the callback directory.
+    for (size_t i = 0; i < OriginalTLS.size(); ++i)
+        Writeptr((Directory->AddressOfCallBacks + i * sizeof(size_t)), OriginalTLS[i]);
+
+    // If the original had callbacks, we need to call the first one.
+    if (auto Callback = (size_t *)Directory->AddressOfCallBacks; *(size_t *)Callback)
+        ((decltype(TLSCallback) *)Callback)(a, b, c);
 }
 
 // Sometimes plugins want to name their threads, and not all games support that..
 LONG WINAPI Threadname(PEXCEPTION_POINTERS Info)
 {
-    if(Info->ExceptionRecord->ExceptionCode == 0x406D1388)
+    if (Info->ExceptionRecord->ExceptionCode == 0x406D1388)
         return EXCEPTION_CONTINUE_EXECUTION;
     else
         return EXCEPTION_CONTINUE_SEARCH;
 }
 
-// Called after Ntdll.ldr's ctors but before main.
-void WINAPI TLSCallback(PVOID a, DWORD b, PVOID c)
-{
-    // Sometimes plugins want to name their threads, and not all games support that..
-    AddVectoredExceptionHandler(0, Threadname);
-
-    // Fixup the TLS callbacks.
-    if(const auto Address = AddressofTLS())
-    {
-        auto Protection = Memprotect::Unprotectrange(Address, sizeof(size_t));
-        {
-            // Remove the TLS callback in-case plugins spawn new threads.
-            *(size_t *)Address = 0;
-
-            // Check all the places plugins may reside.
-            Loadallplugins();
-
-            // Restore the original TLS callback.
-            *(size_t *)Address = OriginalTLS;
-        }
-        Memprotect::Protectrange(Address, sizeof(size_t), Protection);
-    }
-    else
-    {
-        // Check all the places plugins may reside.
-        Loadallplugins();
-    }
-
-    // If there was a TLS callback already, call it directly.
-    if(OriginalTLS) return ((decltype(TLSCallback) *)OriginalTLS)(a, b, c);
-}
-
 // Entrypoint when loaded as a shared library.
 BOOLEAN WINAPI DllMain(HINSTANCE hDllHandle, DWORD nReason, LPVOID lpvReserved)
 {
-    if(nReason == DLL_PROCESS_ATTACH)
+    if (nReason == DLL_PROCESS_ATTACH)
     {
         // Ensure that Ayrias default directories exist.
         (void)_mkdir("./Ayria/");
@@ -82,27 +74,27 @@ BOOLEAN WINAPI DllMain(HINSTANCE hDllHandle, DWORD nReason, LPVOID lpvReserved)
         // Only keep a log for this session.
         Logging::Clearlog();
 
-        // Set TLS to run our callback when fully loaded.
-        if(const auto Address = AddressofTLS())
+        // Save the callbacks and add ours.
+        const auto Directory = getTLSDirectory();
+        auto Callback = (size_t *)Directory->AddressOfCallBacks;
+        if (*Callback) { OriginalTLS.push_back(*Callback); Writeptr(Callback, TLSCallback); }
+        else
         {
-            auto Protection = Memprotect::Unprotectrange(Address, sizeof(size_t));
+            do
             {
-                OriginalTLS = *(size_t *)Address;
-                *(size_t *)Address = (size_t)TLSCallback;
-            }
-            Memprotect::Protectrange(Address, sizeof(size_t), Protection);
+                OriginalTLS.push_back(*Callback);
+                Writeptr(Callback, Dummycallback);
+            } while (*(++Callback));
 
-            // Opt out of further notifications.
-            DisableThreadLibraryCalls(hDllHandle);
+            Callback--;
+            Writeptr(Callback, TLSCallback);
         }
-    }
-    if(nReason == DLL_THREAD_ATTACH)
-    {
+
+        // Sometimes plugins want to name their threads, and not all games support that..
+        AddVectoredExceptionHandler(0, Threadname);
+
         // Opt out of further notifications.
         DisableThreadLibraryCalls(hDllHandle);
-
-        // Call TLS on the first thread as a fallback.
-        TLSCallback(hDllHandle, nReason, lpvReserved);
     }
 
     return TRUE;
@@ -165,6 +157,6 @@ void Loadallplugins()
     for (const auto &Item : Freshplugins)
     {
         auto Callback = GetProcAddress(HMODULE(Item), "onStartup");
-        if(Callback) (reinterpret_cast<void (*)(bool)>(Callback))(false);
+        if (Callback) (reinterpret_cast<void (*)(bool)>(Callback))(false);
     }
 }
