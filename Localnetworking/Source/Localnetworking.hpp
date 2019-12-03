984e1251131065c2af368589255d44a00de88874
Convery <Convery10122@gmail.com>
2019-12-03 12:01:53 +0100

    Initial version of localnet backend.

---
+++ Localnetworking/Source/Localnetworking.hpp
@@ -0,0 +1,26 @@
+/*
+    Initial author: Convery (tcn@ayria.se)
+    Started: 2019-12-03
+    License: MIT
+*/
+
+#pragma once
+#include "IServer.hpp"
+#include <Stdinclude.hpp>
+
+namespace Localnetworking
+{
+    extern uint16_t Backendport;
+
+    // Whether or not a plugin claims ownership of this host.
+    bool isProxiedhost(std::string_view Hostname);
+
+    // Notify the backend about a client-port that will connect to us.
+    void Associateport(std::string_view Hostname, uint16_t Port);
+
+    // A datagram socket, which internally is sent over TCP.
+    size_t Createsocket(IServer::Address_t Serveraddress, std::string_view Hostname);
+
+    // Initialize the server backends, only TCP and UDP for now.
+    void Createbackend(uint16_t Serverport);
+}
