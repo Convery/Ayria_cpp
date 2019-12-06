23848d0cf1b258bd64c9b4a672a66762c73b08cc
Convery <Convery10122@gmail.com>
2019-12-06 09:16:36 +0100

    Merge fix.... again..

--- Utilities/FNV1Hash.hpp
+++ Utilities/FNV1Hash.hpp
@@ -138,3 +138,12 @@ namespace Hash
         return FNV1a_64(String.data(), String.size());
     }
 }
+
+// Drop-in generic functions for std:: algorithms, containers, and such.
+// e.g. std::unordered_map<SillyType, int, decltype(FNV::Hash), decltype(FNV::Equal)>
+namespace FNV
+{
+    constexpr inline auto Hash = [](const auto &v) { return Hash::FNV1a_64(&v, sizeof(v)); };
+    constexpr inline auto Equal = [](const auto &l, const auto &r) { return Hash(l) == Hash(r); };
+}
+
