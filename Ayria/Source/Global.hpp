20149099feaee5b89f36f291d0ee454b0cf68060
Convery <Convery10122@gmail.com>
2020-04-17 10:36:17 +0200

    Started implementing networking for Ayria.

--- Ayria/Source/Global.hpp
+++ Ayria/Source/Global.hpp
@@ -20,7 +20,7 @@
 
 
 
-    export bool __cdecl onRequest(char *Request, char **Response); 
+    export bool __cdecl onRequest(char *Request, char **Response);
 
 
 
@@ -65,52 +65,51 @@
 */
 
 
+//
+//namespace Networking
+//{
+//    using NodeID = uint64_t;
+//    std::vector<NodeID> getNodes();
+//
+//}
+//
+//
+//
+//namespace IPC
+//{
+//    constexpr size_t Instancecount = 12;
+//
+//    using Request_t = struct { std::string Subject, Content; };
+//    using Callback_t = std::function<bool(const Request_t &Request, std::string &Response)>;
+//
+//    void addBroadcast(std::string_view Subject, std::string_view Content);
+//    void addHandler(std::string_view Subject, Callback_t Callback);
+//    void addBroadcast(Request_t &&Message);
+//
+//    extern "C" EXPORT_ATTR void Broadcast(const char *Subject, const char *Content);
+//}
+//
+//namespace Console
+//{
+//
+//    extern "C" EXPORT_ATTR void Consoleprint(const char *);
+//    extern "C" EXPORT_ATTR void Consoleexec(const char *);
+//}
+
 
 namespace Networking
 {
-    using NodeID = uint64_t;
-    std::vector<NodeID> getNodes();
-
-}
-
-
-
-namespace IPC
-{
-    constexpr size_t Instancecount = 12;
-
     using Request_t = struct { std::string Subject, Content; };
-    using Callback_t = std::function<bool(const Request_t &Request, std::string &Response)>;
+    using Callback_t = std::function<void(const Request_t &Request, std::string &Response)>;
 
     void addBroadcast(std::string_view Subject, std::string_view Content);
     void addHandler(std::string_view Subject, Callback_t Callback);
     void addBroadcast(Request_t &&Message);
-
-    extern "C" EXPORT_ATTR void Broadcast(const char *Subject, const char *Content);
+    void onFrame();
 }
 
-namespace Console
-{
-
-    extern "C" EXPORT_ATTR void Consoleprint(const char *);
-    extern "C" EXPORT_ATTR void Consoleexec(const char *);
-}
-
-
-namespace Networking
-{
-    using Request_t = struct { std::string Subject, Content; };
-    using Callback_t = std::function<bool(const Request_t &Request, std::string &Response)>;
-
-    void addBroadcast(std::string_view Subject, std::string_view Content);
-    void addHandler(std::string_view Subject, Callback_t Callback);
-    void addBroadcast(Request_t &&Message);
-
-    extern "C" EXPORT_ATTR unsigned short __cdecl getNetworkport();
-}
-
-namespace Console
-{
-
-}
-
+//namespace Console
+//{
+//
+//}
+//
