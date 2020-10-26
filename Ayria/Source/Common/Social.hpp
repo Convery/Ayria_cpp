7d3064131908429e2c2bcee81b9a943c71cd799e
Convery <Convery10122@gmail.com>
2020-10-26 08:43:38 +0100

    Social system Rework.

--- Ayria/Source/Common/Social.hpp
+++ Ayria/Source/Common/Social.hpp
@@ -9,119 +9,187 @@
 
 namespace Social
 {
-    // Returns false if clients are offline or missing public key.
-    bool Sendinstantmessage(std::vector<uint32_t> Clients, uint32_t Messagetype, std::string_view Message);
-    bool Sendinstantmessage_enc(uint32_t Client, uint32_t Messagetype, std::string_view Message);
+    using Message_t = struct { uint32_t Timestamp; AccountID_t Source; std::u8string Message; };
+    using Relation_t = struct { uint32_t AccountID; std::u8string Username; uint32_t Flags; };
+    using Relationflags_t = union
+    {
+        uint32_t Raw;
+        struct
+        {
+            uint32_t
+                isFriend : 1,
+                isBlocked : 1,
+                isFollower : 1,
+                isGroupmember : 1,
+                AYA_RESERVED1 : 1,
+                AYA_RESERVED2 : 1,
+                AYA_RESERVED3 : 1;
+        };
+    };
 
-    // Returns the chat messages seen this session.
-    using Message_t = struct { uint32_t Sender; uint32_t Type; std::string Message; };
-    std::vector<Message_t> Readinstantmessages(uint32_t Offset, uint32_t Count, uint32_t Messagetype, uint32_t SenderID = 0);
+    namespace Messaging
+    {
+        namespace Read
+        {
+            std::vector<Message_t *> All();
+            std::vector<Message_t *> bySenders(const std::vector<uint32_t> &SenderIDs);
+            std::vector<Message_t *> byTime(uint32_t First, uint32_t Last, uint32_t SenderID = 0);
+        }
+        namespace Send
+        {
+            // Returns false if clients are offline or missing public key.
+            bool toClient(uint32_t ClientID, std::u8string_view Message);
+            bool toClientencrypted(uint32_t ClientID, std::u8string_view Message);
+            bool Broadcast(std::vector<uint32_t> ClientIDs, std::u8string_view Message);
+        }
 
-    // Requests key from Clients, shares own.
-    void Syncpublickeys(std::vector<uint32_t> Clients);
+        // Current size of the storage.
+        size_t getMessagecount();
 
-    // Add the message-handlers.
-    void Initialize();
+        // Add the message-handlers.
+        void Initialize();
+    }
 
-    // Just saves and modifies the friendslist. Use Clientinfo::getNetworkclients to check online status.
-    using Userinfo_t = struct { uint32_t UserID; std::string Username; };
-    void addFriend(uint32_t UserID, std::string_view Username);
-    const std::vector<Userinfo_t> *getFriendslist();
-    void removeFriend(uint32_t UserID);
+    namespace Relations
+    {
+        void Add(uint32_t UserID, std::u8string_view Username, uint32_t Relationflags);
+        void Remove(uint32_t UserID, std::u8string_view Username);
+        const std::vector<Relation_t> *Get();
+
+        // Disk management.
+        void Load(std::wstring_view Path = L"./Ayria/Relations.json"s);
+        void Save(std::wstring_view Path = L"./Ayria/Relations.json"s);
+    }
 
     // Add API handlers.
-    inline std::string __cdecl addFriend(const char *JSONString)
-    {
-        do
-        {
-            if (!JSONString) break;
-            const auto Object = ParseJSON(JSONString);
-            if (!Object.contains("UserID")) break;
-
-            addFriend(Object["UserID"], Object.value("Username", "Unknown"));
-        } while (false);
-
-        return "{}";
-    }
     inline std::string __cdecl removeFriend(const char *JSONString)
     {
-        do
-        {
-            if (!JSONString) break;
-            const auto Object = ParseJSON(JSONString);
-            if (!Object.contains("UserID")) break;
+        const auto Object = ParseJSON(JSONString);
+        const auto UserID = Object.value("UserID", uint32_t());
+        const auto Username = Object.value("Username", std::u8string());
 
-            removeFriend(Object["UserID"]);
-        } while (false);
+        // We need an ID or username.
+        if (UserID == 0 && Username.empty()) [[unlikely]] return "{ \"Status\": \"Error\" }";
 
-        return "{}";
+        Relations::Remove(UserID, Username);
+        return "{ \"Status\": \"OK\" }";
+    }
+    inline std::string __cdecl addFriend(const char *JSONString)
+    {
+        const auto Object = ParseJSON(JSONString);
+        const auto Flags = Object.value("Flags", uint32_t());
+        const auto UserID = Object.value("UserID", uint32_t());
+        const auto Username = Object.value("Username", std::u8string());
+
+        // We need an ID, the username is optional.
+        if (UserID == 0) [[unlikely]] return "{ \"Status\": \"Error\" }";
+
+        Relationflags_t Internal{ Flags }; Internal.isFriend = true;
+        Relations::Add(UserID, Username, Internal.Raw);
+        return "{ \"Status\": \"OK\" }";
+    }
+    inline std::string __cdecl blockUser(const char *JSONString)
+    {
+        const auto Object = ParseJSON(JSONString);
+        const auto Flags = Object.value("Flags", uint32_t());
+        const auto UserID = Object.value("UserID", uint32_t());
+        const auto Username = Object.value("Username", std::u8string());
+
+        // We need an ID, the username is optional.
+        if (UserID == 0) [[unlikely]] return "{ \"Status\": \"Error\" }";
+
+        Relationflags_t Internal{ Flags }; Internal.isBlocked = true;
+        Relations::Add(UserID, Username, Internal.Raw);
+        return "{ \"Status\": \"OK\" }";
     }
     inline std::string __cdecl Friendslist(const char *)
     {
-        const auto Friends = getFriendslist();
         auto Array = nlohmann::json::array();
-
-        for (const auto &Item : *Friends)
-            Array += { { "Username", Item.Username }, { "UserID", Item.UserID } };
-
-        return Array.dump(4);
-    }
-
-    inline std::string __cdecl ReadIM(const char *JSONString)
-    {
-        const auto Request = ParseJSON(JSONString);
-        const auto Type = Request.value("Type", uint32_t());
-        const auto Count = Request.value("Count", uint32_t(1));
-        const auto Sender = Request.value("Sender", uint32_t());
-        const auto Offset = Request.value("Offset", uint32_t());
-
-        auto Array = nlohmann::json::array();
-        for (const auto &Message : Readinstantmessages(Offset, Count, Type, Sender))
+        for (const auto &[ID, Username, Flags] : *Relations::Get())
         {
-            auto Object = nlohmann::json::object();
-            Object["Message"] = Message.Message;
-            Object["Sender"] = Message.Sender;
-            Object["Type"] = Message.Type;
-            Array += Object;
+            const Relationflags_t Internal{ Flags };
+            if (Internal.isFriend)
+            {
+                Array += { { "Username", Username }, { "UserID", ID }, { "Flags", Flags }};
+            }
         }
 
-        return Array.dump();
+        return DumpJSON(Array);
     }
-    inline std::string __cdecl SendIM(const char *JSONString)
-    {
-        const auto Request = ParseJSON(JSONString);
-        const auto Type = Request.value("Type", uint32_t());
-        const auto Message = Request.value("Message", std::string());
-        const auto Clients = Request.value("Clients", std::vector<uint32_t>());
 
-        if (Sendinstantmessage(Clients, Type, Message))
-            return "{}";
+    inline std::string __cdecl Readmessages(const char *JSONString)
+    {
+        const auto Object = ParseJSON(JSONString);
+        const auto Endtime = Object.value("Endtime", uint32_t());
+        const auto SenderID = Object.value("SenderID", uint32_t());
+        const auto Starttime = Object.value("Starttime", uint32_t());
+        auto SenderIDs = Object.value("SenderIDs", std::vector<uint32_t>());
+
+        std::vector<Message_t *> Messages;
+        if (Starttime + Endtime == 0)
+        {
+            SenderIDs.push_back(SenderID);
+            Messages = Messaging::Read::bySenders(SenderIDs);
+        }
         else
-            return "{ \"Error\": \"All clients are offline\" }";
+        {
+            Messages = Messaging::Read::byTime(Starttime, Endtime, SenderID);
+        }
+
+        auto Array = nlohmann::json::array();
+        for (const auto Pointer : Messages)
+        {
+            const auto &[Timestamp, Source, Message] = *Pointer;
+            Array += { { "Timestamp", Timestamp }, { "Source", Source.Raw }, { "Message", Message } };
+        }
+
+        return DumpJSON(Array);
     }
-    inline std::string __cdecl SendIM_enc(const char *JSONString)
+    inline std::string __cdecl Sendmessage(const char *JSONString)
     {
-        const auto Request = ParseJSON(JSONString);
-        const auto Type = Request.value("Type", uint32_t());
-        const auto Client = Request.value("Client", uint32_t());
-        const auto Message = Request.value("Message", std::string());
+        const auto Object = ParseJSON(JSONString);
+        const auto Target = Object.value("Target", uint32_t());
+        const auto Message = Object.value("Message", std::u8string());
+        auto Targets = Object.value("Targets", std::vector<uint32_t>());
 
-        if (Sendinstantmessage_enc(Client, Type, Message))
-            return "{}";
+        if (!Targets.empty())
+        {
+            Targets.push_back(Target);
+            if(Messaging::Send::Broadcast(Targets, Message))
+                return "{ \"Status\": \"OK\" }";
+        }
         else
-            return "{ \"Error\": \"Client is offline or missing pub-key, try again later.\" }";
+        {
+            if (Messaging::Send::toClient(Target, Message))
+                return "{ \"Status\": \"OK\" }";
+        }
+
+        return "{ \"Status\": \"Error\" }";
+    }
+    inline std::string __cdecl Sendmessage_enc(const char *JSONString)
+    {
+        const auto Object = ParseJSON(JSONString);
+        const auto Target = Object.value("Target", uint32_t());
+        const auto Message = Object.value("Message", std::u8string());
+
+        if (Messaging::Send::toClientencrypted(Target, Message))
+            return "{ \"Status\": \"OK\" }";
+        else
+            return "{ \"Status\": \"Error\" }";
     }
 
     inline void API_Initialize()
     {
         API::Registerhandler_Social("addFriend", addFriend);
+        API::Registerhandler_Social("blockUser", blockUser);
         API::Registerhandler_Social("Friendslist", Friendslist);
         API::Registerhandler_Social("removeFriend", removeFriend);
 
-        API::Registerhandler_Social("ReadIM", ReadIM);
-        API::Registerhandler_Social("SendIM", SendIM);
-        API::Registerhandler_Social("SendIM_enc", SendIM_enc);
+        API::Registerhandler_Social("Sendmessage", Sendmessage);
+        API::Registerhandler_Social("Readmessages", Readmessages);
+        API::Registerhandler_Social("Sendmessage_enc", Sendmessage_enc);
 
-        Initialize();
+        Messaging::Initialize();
+        Relations::Load();
     }
 }
