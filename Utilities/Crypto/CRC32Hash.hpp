be32836ebc4375f275365059f334a718a7cf631f
Convery <Convery10122@gmail.com>
2021-03-23 21:08:27 +0100

    Cleanup of CRC32.

--- Utilities/Crypto/CRC32Hash.hpp
+++ Utilities/Crypto/CRC32Hash.hpp
@@ -3,9 +3,7 @@
     Started: 2020-02-10
     License: MIT
 
-    // 32A = 0x04C11DB7
-    // 32B = 0xEDB88320
-    // 32T = Tencents alternative
+    // CRC32T = Tencents alternative to CRC32B
 */
 
 #pragma once
@@ -13,71 +11,69 @@
 
 namespace Hash
 {
-    namespace Internal
+    namespace CRCInternal
     {
-        constexpr std::array<uint32_t, 256> CRC32B_Table()
+        template <typename T> concept Bytealigned_t = sizeof(T) == 1;
+
+        template <Bytealigned_t T, uint32_t Polynomial, bool Shiftright>
+        constexpr uint32_t CRC32(const T *Input, size_t Length, uint32_t IV)
         {
-            const uint32_t Polynomial = 0xEDB88320;
-            std::array<uint32_t, 256> Table{};
+            uint32_t CRC = IV;
 
-            for (uint16_t i = 0; i < 0xFF; ++i)
+            for (size_t i = 0; i < Length; ++i)
             {
-                uint32_t Remainder{ i };
+                if constexpr (Shiftright) CRC ^= Input[i];
+                else CRC ^= Input[i] << 24;
 
-                for (uint8_t b = 8; !!b; --b)
+                for (size_t b = 0; b < 8; ++b)
                 {
-                    if (!(Remainder & 1)) Remainder >>= 1;
-                    else Remainder = (Remainder >> 1) ^ Polynomial;
+                    if constexpr (Shiftright)
+                    {
+                        if (!(CRC & 1)) CRC >>= 1;
+                        else CRC = (CRC >> 1) ^ Polynomial;
+                    }
+                    else
+                    {
+                        if (!(CRC & (1UL << 31))) CRC <<= 1;
+                        else CRC = (CRC << 1) ^ Polynomial;
+                    }
                 }
-
-                Table[i] = Remainder;
             }
 
-            return Table;
+            return ~CRC;
         }
     }
 
     // Compile-time hashing for fixed-length datablocks.
-    [[nodiscard]] constexpr uint32_t CRC32A(const char *Input, const uint32_t Length)
+    template <CRCInternal::Bytealigned_t T> [[nodiscard]] constexpr uint32_t CRC32A(const T *Input, size_t Length)
     {
-        const uint32_t Polynomial = 0x04C11DB7;
-        uint32_t Block = 0xFFFFFFFF;
-
-        for (uint32_t i = 0; i < Length; ++i)
-        {
-            Block ^= Input[i] << 24;
-
-            for (uint8_t b = 0; b < 8; ++b)
-            {
-                if (!(Block & (1UL << 31))) Block <<= 1;
-                else Block = (Block << 1) ^ Polynomial;
-            }
-        }
-
-        return ~Block;
+        return CRCInternal::CRC32<T, 0x04C11DB7, false>(Input, Length, 0xFFFFFFFF);
     }
-    [[nodiscard]] constexpr uint32_t CRC32B(const char *Input, const uint32_t Length)
+    template <CRCInternal::Bytealigned_t T> [[nodiscard]] constexpr uint32_t CRC32B(const T *Input, size_t Length)
     {
-        uint32_t Block = 0xFFFFFFFF;
-        for (uint32_t i = 0; i < Length; ++i)
-        {
-            Block = Internal::CRC32B_Table()[(Block ^ Input[i]) & 0xFF] ^ (Block >> 8);
-        }
-
-        return ~Block;
+        return CRCInternal::CRC32<T, 0xEDB88320, true>(Input, Length, 0xFFFFFFFF);
     }
-    [[nodiscard]] constexpr uint32_t CRC32T(const char *Input, const uint32_t Length)
+    template <CRCInternal::Bytealigned_t T> [[nodiscard]] constexpr uint32_t CRC32T(const T *Input, size_t Length)
     {
-        uint32_t Block = ~Length;
-        for (uint32_t i = 0; i < Length; ++i)
-        {
-            Block = Internal::CRC32B_Table()[(Block ^ Input[i]) & 0xFF] ^ (Block >> 8);
-        }
-
-        return ~Block;
+        return CRCInternal::CRC32<T, 0xEDB88320, true>(Input, Length, ~uint32_t(Length));
     }
 
-    static_assert(CRC32A("123456789", 9) == 0xFC891918, "CRC32-A is broken =(");
-    static_assert(CRC32B("123456789", 9) == 0xCBF43926, "CRC32-B is broken =(");
-    static_assert(CRC32T("123456789", 9) == 0x67578F7D, "CRC32-T is broken =(");
+    // Run-time hashing for fixed-length datablocks, constexpr depending on compiler.
+    template <typename T> [[nodiscard]] constexpr uint32_t CRC32A(const T *Input, size_t Length)
+    {
+        return CRCInternal::CRC32<uint8_t, 0x04C11DB7, false>((const uint8_t *)Input, sizeof(T) * Length, 0xFFFFFFFF);
+    }
+    template <typename T> [[nodiscard]] constexpr uint32_t CRC32B(const T *Input, size_t Length)
+    {
+        return CRCInternal::CRC32<uint8_t, 0xEDB88320, true>((const uint8_t *)Input, sizeof(T) * Length, 0xFFFFFFFF);
+    }
+    template <typename T> [[nodiscard]] constexpr uint32_t CRC32T(const T *Input, size_t Length)
+    {
+        return CRCInternal::CRC32<uint8_t, 0xEDB88320, true>((const uint8_t *)Input, sizeof(T) * Length, ~uint32_t(Length));
+    }
+
+    // Sanity checking.
+    static_assert(CRC32A("123456789", 9U) == 0xFC891918, "CRC32-A is broken =(");
+    static_assert(CRC32B("123456789", 9U) == 0xCBF43926, "CRC32-B is broken =(");
+    static_assert(CRC32T("123456789", 9U) == 0x67578F7D, "CRC32-T is broken =(");
 }
