988bd1de2a2ee81a4a5316d2dac4641c0638b44c
Convery <tcn@ayria.se>
2021-08-30 02:32:49 +0200

    Simplified the global state, adding helpers to get IDs.

--- Ayria/Source/Global.hpp
+++ Ayria/Source/Global.hpp
@@ -9,22 +9,6 @@
 
 // Keep global data together, but remember to keep natural alignment for pointers!
 #pragma pack(push, 1)
-struct AccountID_t
-{
-    uint32_t AyriaID{}; // Official ID for identification.
-    uint32_t KeyID{};   // WW32 hash of the public signing key.
-
-    AccountID_t() = default;
-    AccountID_t(uint64_t Raw)
-    {
-        AyriaID = (Raw >> 32) & 0xFFFFFFFF;
-        KeyID = Raw & 0xFFFFFFFF;
-    }
-    operator uint64_t () const
-    {
-        return { uint64_t(AyriaID) << 32 | KeyID };
-    }
-};
 struct alignas(64) Globalstate_t
 {
     // Helper to initialize the pointers in the same region.
@@ -34,18 +18,17 @@ struct alignas(64) Globalstate_t
 
     uint32_t InternalIP{}, ExternalIP{};    // From LANNetworking and STUN.
     uint32_t GameID{}, ModID{};             // Set through Platform-wrapper.
-    AccountID_t AccountID{};
-    // 24 bytes.
+    // 16 bytes.
 
     // 25519-curve cryptography.
     std::unique_ptr<std::array<uint8_t, 32>> SigningkeyPublic{ Allocate<std::array<uint8_t, 32>>() };     // Shared static key.
     std::unique_ptr<std::array<uint8_t, 64>> SigningkeyPrivate{ Allocate<std::array<uint8_t, 64>>() };    // Derived from authentication.
     std::unique_ptr<std::array<uint8_t, 32>> EncryptionkeyPrivate{ Allocate<std::array<uint8_t, 32>>() }; // Transient session-key.
-    // 36 / 48 bytes.
+    // 28 / 40 bytes.
 
     // Rarely used (for now), but good to have in the future.
     std::unique_ptr<std::pmr::u8string> Username{ Allocate<std::pmr::u8string>(&Internal) };
-    // 40 / 56 bytes.
+    // 32 / 48 bytes.
 
     // Internal settings, need packing (line 11) or we'll get 6 bytes of padding.
     struct
@@ -55,6 +38,7 @@ struct alignas(64) Globalstate_t
             enableExternalconsole : 1,
             enableIATHooking : 1,
             enableFileshare : 1,
+            isAuthenticated : 1,
             modifiedConfig : 1,
             noNetworking : 1,
 
@@ -66,14 +50,18 @@ struct alignas(64) Globalstate_t
             isHosting : 1,
             isIngame : 1,
 
-            // 7 bits available.
+            // 6 bits available.
             END : 1;
     } Settings{};
-    // 42 / 58 bytes.
+    // 34 / 50 bytes.
 
     // ************************************
-    // 22 / 6 bytes available for use here.
+    // 30 / 14 bytes available for use here.
     // ************************************
+
+    // Helpers for access to the members.
+    std::string getLongID() const { return Base58::Encode(*SigningkeyPublic); }
+    uint32_t getShortID() const { return Hash::WW32(*SigningkeyPublic); }
 
     // Struct alignment causes intentional padding.
     #pragma warning(suppress : 4324)
@@ -86,3 +74,4 @@ extern Globalstate_t Global;
 
 // Project includes.
 #include "Backend\AABackend.hpp"
+#include "Services\AAServices.hpp"
