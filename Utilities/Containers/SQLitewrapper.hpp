4759931cb19b2c29afa32f5a48c0e47700faa1eb
Convery <tcn@ayria.se>
2021-12-07 00:07:58 +0100

    Readability, safety, and correctness improvements for SQLite.

--- Utilities/Containers/SQLitewrapper.hpp
+++ Utilities/Containers/SQLitewrapper.hpp
@@ -2,6 +2,20 @@
     Initial author: Convery (tcn@ayria.se)
     Started: 2021-11-15
     License: MIT
+
+    Largely inspired by https://github.com/SqliteModernCpp/sqlite_modern_cpp
+
+    NOTES:
+    On errors, an assert is triggered.
+    If a query fails, no output is written.
+    If a query returns incompatible data, output is default constructed.
+    If a lambda is supplied as output, it will be called for each result but can return false to terminate.
+    If an output is not provided, the query will be executed when the object goes out of scope but results ignored.
+
+    Database_t(*Ptr) << "SELECT * FROM Table WHERE Input = ?;" << myInput
+        >> [](Results...) {};
+        >> std::tie(a, b);
+        >> myOutput;
 */
 
 #pragma once
@@ -15,20 +29,28 @@
 #include <type_traits>
 #include <string_view>
 
+// For UTF8 encoding.
+#include "../Encoding/Stringconv.hpp"
+
 #include <sqlite3.h>
 #pragma comment(lib, "sqlite3.lib")
 
 namespace sqlite
 {
+    // Ignore warnings about unreachable code.
+    #pragma warning(push)
+    #pragma warning(disable: 4702)
+
     // Helpers for type deduction.
     template <class, template <class...> class> inline constexpr bool isDerived = false;
-    template< template <class...> class T, class... Args> inline constexpr bool isDerived<T<Args...>, T> = true;
+    template <template <class...> class T, class... Args> inline constexpr bool isDerived<T<Args...>, T> = true;
 
-    template <typename T> concept SQLBlob = isDerived<T, std::vector>;
-    template <typename T> concept SQLOptional = isDerived<T, std::optional>;
-    template <typename T> concept SQLString = isDerived<T, std::basic_string>;
-    template <typename T> concept SQLNumber = std::is_integral_v<T> || std::is_floating_point_v<T>;
-    template <typename T> concept SQLValue = SQLNumber<T> || SQLString<T> || SQLOptional<T> || SQLBlob<T> || std::is_same_v<T, nullptr_t>;
+    template <typename T> concept Integer_t = std::is_integral_v<T>;
+    template <typename T> concept Float_t = std::is_floating_point_v<T>;
+    template <typename T> concept Optional_t = isDerived<T, std::optional>;
+    template <typename T> concept String_t = isDerived<T, std::basic_string> || isDerived<T, std::basic_string_view>;
+    template <typename T> concept Blob_t = isDerived<T, std::vector> || isDerived<T, std::set> || isDerived<T, std::unordered_set>;
+    template <typename T> concept Value_t = Integer_t<T> || String_t<T> || Optional_t<T> || Blob_t<T> || Float_t<T> || std::is_same_v<T, nullptr_t> ;
 
     // Helper to get the arguments to a function.
     template <typename> struct Functiontraits;
@@ -45,31 +67,46 @@ namespace sqlite
     };
 
     // Convert between SQL and C++.
-    template <SQLValue T> T getResult(sqlite3_stmt *Statement, int Index)
+    template <Value_t T> T getResult(sqlite3_stmt *Statement, int Index)
     {
-        switch (sqlite3_column_type(Statement, Index))
-        {
-            case SQLITE_NULL: return {};
+        const auto Type = sqlite3_column_type(Statement, Index);
+        if (SQLITE_NULL == Type) return {};
 
+        if constexpr (Optional_t<T>)
+        {
+            return { getResult<typename T::value_type>(Statement, Index) };
+        }
+
+        switch (Type)
+        {
             case SQLITE_INTEGER:
-                if constexpr (std::is_integral_v<T> && sizeof(T) == sizeof(uint64_t)) return sqlite3_column_int64(Statement, Index);
-                if constexpr (std::is_integral_v<T> && sizeof(T) != sizeof(uint64_t)) return sqlite3_column_int(Statement, Index);
+                if constexpr (Integer_t<T>) return sqlite3_column_int64(Statement, Index);
                 break;
 
             case SQLITE_FLOAT:
-                if constexpr (std::is_floating_point_v<T>) return sqlite3_column_double(Statement, Index);
+                if constexpr (Float_t<T>) return sqlite3_column_double(Statement, Index);
                 break;
 
             case SQLITE_TEXT:
-                if constexpr (SQLString<T>) return T((typename T::value_type *)sqlite3_column_text(Statement, Index));
+                if constexpr (isDerived<T, std::basic_string>)
+                {
+                    const std::u8string Temp = (char8_t *)sqlite3_column_text(Statement, Index);
+
+                    if constexpr (std::is_same_v<typename T::value_type, char>) return Encoding::toNarrow(Temp);
+                    else if constexpr (std::is_same_v<typename T::value_type, wchar_t>) return Encoding::toWide(Temp);
+                    else return Temp;
+                }
+                static_assert(!isDerived<T, std::basic_string_view>, "Can't bind result to string_view.");
                 break;
 
             case SQLITE_BLOB:
-                if constexpr (SQLBlob<T>)
+                if constexpr (Blob_t<T>)
                 {
                     const auto Size = sqlite3_column_bytes(Statement, Index);
                     const auto Buffer = (typename T::value_type *)sqlite3_column_blob(Statement, Index);
-                    return T(Buffer, Buffer + Size / sizeof(typename T::value_type));
+
+                    const std::span Temp(Buffer, Size / sizeof(typename T::value_type));
+                    return T(Temp.begin(), Temp.end());
                 }
                 break;
         }
@@ -78,22 +115,39 @@ namespace sqlite
         assert(false);
         return {};
     }
-    template <SQLValue T> void getResult(sqlite3_stmt *Statement, int Index, T &Output)
+    template <Value_t T> void getResult(sqlite3_stmt *Statement, int Index, T &Output)
     {
         Output = getResult<T>(Statement, Index);
     }
-    template <SQLValue T> void bindValue(sqlite3_stmt *Statement, int Index, const T &Value)
+    template <Value_t T> void bindValue(sqlite3_stmt *Statement, int Index, const T &Value)
     {
         const auto Result = [&]() -> int
         {
-            if constexpr (SQLBlob<T>) return sqlite3_bind_blob(Statement, Index, Value.data(), int(Value.size() * sizeof(typename T::value_type)), SQLITE_TRANSIENT);
-            if constexpr (SQLString<T>) return sqlite3_bind_text(Statement, Index, (const char *)Value.data(), (int)Value.size(), SQLITE_TRANSIENT);
-            if constexpr (std::is_integral_v<T> && sizeof(T) == sizeof(uint64_t)) return sqlite3_bind_int64(Statement, Index, Value);
-            if constexpr (std::is_integral_v<T> && sizeof(T) != sizeof(uint64_t)) return sqlite3_bind_int(Statement, Index, Value);
-            if constexpr (std::is_floating_point_v<T>) return sqlite3_bind_double(Statement, Index, Value);
+            if constexpr (Blob_t<T>)
+            {
+                if constexpr (isDerived<T, std::vector>)
+                    return sqlite3_bind_blob(Statement, Index, Value.data(), int(Value.size() * sizeof(typename T::value_type)), SQLITE_TRANSIENT);
+                else
+                {
+                    // Needs contigious memory.
+                    const std::vector<typename T::value_type> Temp(Value.begin(), Value.end());
+                    return sqlite3_bind_blob(Statement, Index, Temp.data(), int(Temp.size() * sizeof(typename T::value_type)), SQLITE_TRANSIENT);
+                }
+            }
+            if constexpr (Integer_t<T> && sizeof(T) == sizeof(uint64_t)) return sqlite3_bind_int64(Statement, Index, Value);
+            if constexpr (Integer_t<T> && sizeof(T) != sizeof(uint64_t)) return sqlite3_bind_int(Statement, Index, Value);
             if constexpr (std::is_same_v<T, nullptr_t>) return sqlite3_bind_null(Statement, Index);
+            if constexpr (Float_t<T>) return sqlite3_bind_double(Statement, Index, Value);
 
-            if constexpr (SQLOptional<T>)
+            if constexpr (isDerived<T, std::basic_string_view> || isDerived<T, std::basic_string>)
+            {
+                // Passthrough for std::string and std::u8string.
+                const auto Temp = Encoding::toUTF8(Value);
+
+                return sqlite3_bind_text(Statement, Index, (const char *)Temp.data(), (int)Temp.size(), SQLITE_TRANSIENT);
+            }
+
+            if constexpr (Optional_t<T>)
             {
                 if (Value) bindValue(Statement, Index, *Value);
                 else bindValue(Statement, Index, nullptr);
@@ -118,54 +172,48 @@ namespace sqlite
     // Helper for callbacks.
     template <size_t Count> struct Functionbinder_t
     {
-        template <typename Function, size_t Index>
-        using Argtype = typename Functiontraits<Function>::template arg<Index>;
+        template <typename Function, size_t Index> using Argtype = typename Functiontraits<Function>::template arg<Index>;
+        template <typename Function> using R = typename Functiontraits<Function>::return_type;
 
         template <typename Function, typename ...Values, size_t Boundry = Count>
-        static void Run(sqlite3_stmt *Statement, Function &&Func, Values&& ...va) requires(sizeof...(Values) < Boundry)
+        static R<Function> Run(sqlite3_stmt *Statement, Function &&Func, Values&& ...va) requires(sizeof...(Values) < Boundry)
         {
             typename std::remove_cv_t<typename std::remove_reference_t<Argtype<Function, sizeof...(Values)>>> value{};
 
             getResult(Statement, sizeof...(Values), value);
 
-            Run<Function>(Statement, Func, std::forward<Values>(va)..., std::move(value));
+            return Run<Function>(Statement, Func, std::forward<Values>(va)..., std::move(value));
         }
 
         template <typename Function, typename ...Values, size_t Boundry = Count>
-        static void Run(sqlite3_stmt *, Function &&Func, Values&& ...va) requires(sizeof...(Values) == Boundry)
+        static R<Function> Run(sqlite3_stmt *, Function &&Func, Values&& ...va) requires(sizeof...(Values) == Boundry)
         {
-            Func(std::move(va)...);
+            return Func(std::move(va)...);
         }
     };
 
     // Holds the prepared statement that we append values to.
-    struct Statement_t
+    class Statement_t
     {
         std::unique_ptr<sqlite3_stmt, decltype(&sqlite3_finalize)> Statement;
         bool isStarted{};
         int32_t Index{};
 
-        void Reset()
+        // Intial state for the bindings.
+        inline void setStarted()
         {
-            //
-            if (isStarted && 0 == Index)
-            {
-                sqlite3_reset(Statement.get());
-                sqlite3_clear_bindings(Statement.get());
-            }
-
-            // State.
-            Index = 0;
             isStarted = true;
+            Index = 0;
         }
+
+        // Step through the query and extract anything interesting.
         void Extractsingle(std::function<void()> Callback)
         {
-            Reset();
+            setStarted();
 
             // Extract a row.
             int Result = sqlite3_step(Statement.get());
-            assert(SQLITE_ROW == Result);
-            Callback();
+            if (SQLITE_ROW == Result) Callback();
 
             // Verify that there's indeed only one row.
             Result = sqlite3_step(Statement.get());
@@ -174,7 +222,7 @@ namespace sqlite
         void Extractmultiple(std::function<void()> Callback)
         {
             int Result;
-            Reset();
+            setStarted();
 
             while (SQLITE_ROW == (Result = sqlite3_step(Statement.get())))
             {
@@ -184,12 +232,31 @@ namespace sqlite
             // If there was an error, report it.
             assert(SQLITE_DONE == Result);
         }
-
-        template <SQLValue T> void operator>>(T &Value)
+        void Extractmultiple(std::function<bool()> Callback)
         {
-            Extractsingle([&Value, this]
+            int Result;
+            setStarted();
+
+            while (SQLITE_ROW == (Result = sqlite3_step(Statement.get())))
             {
-                getResult(Statement.get(), 0, Value);
+                if (!Callback())
+                {
+                    return;
+                }
+            }
+
+            // If there was an error, report it.
+            assert(SQLITE_DONE == Result);
+        }
+
+    public:
+        // Result-extraction opterator, lambdas return false to terminate execution or void for complete queries.
+        template <typename Function> requires (!Value_t<Function>) void operator>>(Function &&Callback)
+        {
+            // Interruptable queries.
+            Extractmultiple([&Callback, this]()
+            {
+                return Functionbinder_t<Functiontraits<Function>::argcount>::Run(Statement.get(), Callback);
             });
         }
         template <typename... T> void operator>>(std::tuple<T...> &&Value)
@@ -199,24 +266,42 @@ namespace sqlite
                 Tuple_t<std::tuple<T...>>::iterate(Statement.get(), Value);
             });
         }
-        template <typename Function> requires (!SQLValue<Function>) void operator>>(Function &&Callback)
+        template <Value_t T> void operator>>(T &Value)
         {
-            Extractmultiple([&Callback, this]()
+            Extractsingle([&Value, this]
             {
-                Functionbinder_t<Functiontraits<Function>::argcount>::Run(Statement.get(), Callback);
+                getResult(Statement.get(), 0, Value);
             });
         }
 
-        template <SQLValue T> inline Statement_t &operator<<(const T &Value)
+        // Input operator, sequential propagating of the '?' placeholders in the query.
+        template <Value_t T> Statement_t &operator<<(const T &Value)
         {
+            // Ensure a clean state,
+            if (isStarted && Index == 0)
+            {
+                sqlite3_reset(Statement.get());
+                sqlite3_clear_bindings(Statement.get());
+            }
+
             bindValue(Statement.get(), Index++, Value);
             return *this;
         }
-        inline Statement_t &operator<<(const std::string &Value)
+        Statement_t &operator<<(std::string_view Value)
         {
+            // Ensure a clean state,
+            if (isStarted && Index == 0) [[unlikely]]
+            {
+                sqlite3_reset(Statement.get());
+                sqlite3_clear_bindings(Statement.get());
+            }
+
             bindValue(Statement.get(), Index++, Value);
             return *this;
         }
+
+        // Reset can also be used to avoid RTTI evaluation.
+        void Reset() { setStarted(); }
 
         explicit Statement_t(std::shared_ptr<sqlite3> Connection, std::string_view SQL) : Statement(nullptr, sqlite3_finalize)
         {
@@ -227,12 +312,6 @@ namespace sqlite
             const auto Result = sqlite3_prepare_v2(Connection.get(), SQL.data(), (int)SQL.size(), &Temp, &Remaining);
             assert(SQLITE_OK == Result);
 
-            // For simplicity, we don't accept more than one statement, i.e. only one ';' allowed.
-            assert(std::all_of(Remaining, SQL.data() + SQL.size(), [](auto Item) { return std::isspace(Item); }));
-
-            // Update the internal state.
-            isStarted = false;
-
             // Save the statement and finalize when we go out of scope.
             Statement = { Temp, sqlite3_finalize };
         }
@@ -240,16 +319,7 @@ namespace sqlite
         ~Statement_t()
         {
             // Need to ensure that the statement was evaluated.
-            if (!isStarted)
-            {
-                int Result;
-                Reset();
-
-                while (SQLITE_ROW == (Result = sqlite3_step(Statement.get()))) {};
-
-                // If there was an error, report it.
-                assert(SQLITE_DONE == Result);
-            }
+            if (!isStarted) Extractmultiple([]() {});
         }
     };
 
@@ -261,7 +331,12 @@ namespace sqlite
         Database_t(std::shared_ptr<sqlite3> Ptr) : Connection(Ptr) {};
         Statement_t operator<<(std::string_view SQL) const
         {
+            // For simplicity, we don't accept more than one statement, i.e. only one ';' allowed.
+            assert(1 == std::ranges::count(SQL, ';'));
             return Statement_t(Connection, SQL);
         }
     };
+
+    // Restore warnings.
+    #pragma warning(pop)
 }
