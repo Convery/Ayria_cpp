162c16c0305d38ed01ca898b78cdf233590706a4
Convery <tcn@ayria.se>
2021-12-16 03:13:27 +0100

    Updated VA to accept std::basic_string.

--- Utilities/Encoding/Variadicstring.hpp
+++ Utilities/Encoding/Variadicstring.hpp
@@ -5,10 +5,10 @@
 */
 
 #pragma once
-#include <memory>
 #include <string_view>
 
-#if defined(HAS_ABSEIL)
+// NOTE(tcn): Abseil does not support std::u8string so it's disabled for the foreseeable future.
+#if 0
 template<typename ... Args> [[nodiscard]] std::string va(std::string_view Format, const Args& ...args)
 {
     return absl::StrFormat(Format, args ...);
@@ -16,41 +16,39 @@ template<typename ... Args> [[nodiscard]] std::string va(std::string_view Format
 template<typename ... Args> [[nodiscard]] std::string va(const char *Format, const Args& ...args)
 {
     return absl::StrFormat(Format, args ...);
-}
-#else
-template<typename ... Args> [[nodiscard]] std::string va(std::string_view Format, const Args& ...args)
-{
-    const auto Size = 1 + std::snprintf(nullptr, 0, Format.data(), args ...);
-    auto Buffer = std::make_unique<char[]>(Size);
-
-    std::snprintf(Buffer.get(), Size, Format.data(), args ...);
-    return Buffer.get();
-}
-template<typename ... Args> [[nodiscard]] std::string va(const char *Format, const Args& ...args)
-{
-    const auto Size = 1 + std::snprintf(nullptr, 0, Format, args ...);
-    auto Buffer = std::make_unique<char[]>(Size);
-
-    std::snprintf(Buffer.get(), Size, Format, args ...);
-    return Buffer.get();
 }
 #endif
 
+// Helpers to deal with std::basic_string.
+template <typename T> decltype(auto) Forward(T &&Arg) { return Arg; }
+template <typename T> decltype(auto) Forward(const T &Arg) { return Arg; }
+template <typename T> concept String_t = requires (const T &Arg) { Arg.c_str(); };
+template <String_t T> decltype(auto) Forward(const T &Arg) { return Arg.c_str(); }
+
+template<typename ... Args> [[nodiscard]] std::string va_impl(std::string_view Format, const Args& ...args)
+{
+    const auto Size = std::snprintf(nullptr, 0, Format.data(), args...);
+    std::string Buffer(Size, '\0');
+
+    std::snprintf(Buffer.data(), Size, Format.data(), args...);
+    return Buffer;
+}
+template<typename ... Args> [[nodiscard]] std::string va(std::string_view Format, const Args& ...args)
+{
+    return va_impl(Format, Forward<Args>(args)...);
+}
+
 #if defined (_WIN32)
+template<typename ... Args> [[nodiscard]] std::wstring va_impl(std::wstring_view Format, const Args& ...args)
+{
+    const auto Size = _snwprintf(nullptr, 0, Format.data(), args...);
+    std::wstring Buffer(Size, L'\0');
+
+    _snwprintf(Buffer.data(), Size, Format.data(), args...);
+    return Buffer;
+}
 template<typename ... Args> [[nodiscard]] std::wstring va(std::wstring_view Format, const Args& ...args)
 {
-    const auto Size = 1 + _snwprintf(nullptr, 0, Format.data(), args ...);
-    auto Buffer = std::make_unique<wchar_t[]>(Size);
-
-    _snwprintf(Buffer.get(), Size, Format.data(), args ...);
-    return Buffer.get();
-}
-template<typename ... Args> [[nodiscard]] std::wstring va(const wchar_t *Format, const Args& ...args)
-{
-    const auto Size = 1 + _snwprintf(nullptr, 0, Format, args ...);
-    auto Buffer = std::make_unique<wchar_t[]>(Size);
-
-    _snwprintf(Buffer.get(), Size, Format, args ...);
-    return Buffer.get();
+    return va_impl(Format, Forward<Args>(args)...);
 }
 #endif
